bb46c7a45d6bef4d9af7995dc071c6efa680eff4
antilles <antilles183@gmail.com>
2024-11-19 21:44:08 -0800

    added priority queue test

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,6 @@
 #include "test_Node.cpp"
 #include "test_Evaluator.cpp"
+#include "test_PriorityQueue.cpp"
 
 
 int main() {
@@ -11,26 +12,42 @@ int main() {
     io.console('#', 80, true);
     std::cout << "\tBEGIN TEST --- NODE \n"; 
     io.console('#', 80, true);
+    io.console('\n', 1);
 
     test_Node();
 
     io.console('-', 80, true);
     std::cout << "\tEND TEST --- NODE \n"; 
     io.console('-', 80, true);
-    io.console('\n', 2);
+    io.console('\n', 1);
 
     //##########################################################################
 
     io.console('#', 80, true);
     std::cout << "\tBEGIN TEST --- EVALUATOR \n"; 
     io.console('#', 80, true);
+    io.console('\n', 1);
 
     test_Evaluator();
 
     io.console('-', 80, true);
     std::cout << "\tEND TEST --- EVALUATOR \n"; 
     io.console('-', 80, true);
-    io.console('\n', 2);
+    io.console('\n', 1);
+
+    //##########################################################################
+
+    io.console('#', 80, true);
+    std::cout << "\tBEGIN TEST --- PRIORITY QUEUE \n"; 
+    io.console('#', 80, true);
+    io.console('\n', 1);
+
+    test_PriorityQueue();
+
+    io.console('-', 80, true);
+    std::cout << "\tEND TEST --- PRIORITY QUEUE \n"; 
+    io.console('-', 80, true);
+    io.console('\n', 1);
 
     //##########################################################################
 
