70697c619f799e490406c195620b1c8a81f99aa9
antilles <antilles183@gmail.com>
2024-11-19 21:42:14 -0800

    initial commit

---
+++ test/test_PriorityQueue.cpp
@@ -0,0 +1,34 @@
+#include "../src/Node.h"
+#include <queue>
+
+
+int test_PriorityQueue() {
+
+    int acc;
+
+    std::priority_queue<Node> pq;
+
+
+    // fill pq
+    for(int i = 1; i < 11; i++)
+    {
+        acc = rand() % 100;
+        printf("pushing with acc == %d\n", acc);
+        pq.push( Node(acc) );
+
+    }
+
+
+    // empty pq to console
+    std::cout << "\npq: ";
+    for(;!pq.empty(); pq.pop())
+        std::cout << pq.top().accuracy() << ' ';
+    std::cout << '\n';
+
+
+    
+
+    std::cout << "\n";
+
+    return 0;
+}
