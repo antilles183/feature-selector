b7fa98c8d58bdf032f243aecda31c15f89f91d35
antilles <antilles183@gmail.com>
2024-11-19 21:45:04 -0800

    wrote operator< and operator> tests

--- test/test_Node.cpp
+++ test/test_Node.cpp
@@ -16,5 +16,31 @@ int test_Node() {
 
     std::cout << "\n";
 
+    Node n2(-1);
+    Node n3(0);
+    Node n4(1);
+    Node n5(99);
+
+    std::cout << "n2(-1) < n3(0) == " << (n2 < n3) << std::endl;
+    std::cout << "n2(-1) > n3(0) == " << (n2 > n3) << std::endl;
+    std::cout << "\n";
+    std::cout << "n3(0) < n4(1) == " << (n3 < n4) << std::endl;
+    std::cout << "n3(0) > n4(1) == " << (n3 > n4) << std::endl;
+    std::cout << "\n";
+    std::cout << "n4(1) < n5(99) == " << (n4 < n5) << std::endl;
+    std::cout << "n4(1) > n5(99) == " << (n4 > n5) << std::endl;
+    std::cout << "\n";
+    std::cout << "n5(99) < n5(99) == " << (n5 < n5) << std::endl;
+    std::cout << "n5(99) > n5(99) == " << (n5 > n5) << std::endl;
+    std::cout << "\n";
+
+    std::cout << "n4.accuracy() == " << n4.accuracy() << '\n';
+    std::cout << "n5.accuracy() == " << n5.accuracy() << '\n';
+    std::cout << "n5 > n4 ? n5 : n4 == " << (n5 > n4 ? n5.accuracy() : n4.accuracy()) << std::endl;
+    std::cout << "\n";
+
+
+
+
     return 0;
 }
