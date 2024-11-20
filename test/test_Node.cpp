b97695a8dd6664dc3b30fe41034c016316fd191d
antilles <antilles183@gmail.com>
2024-11-20 11:56:49 -0800

    divided accuracy() by 100 to undo percentage translation

--- test/test_Node.cpp
+++ test/test_Node.cpp
@@ -12,7 +12,7 @@ int test_Node() {
 
     std::cout << "\n";
     std::cout << n1 << std::endl;
-    std::cout << "Accuracy: " << n1.accuracy() << "\n";
+    std::cout << "Accuracy: " << n1.accuracy() / 100 << "\n";
 
     std::cout << "\n";
 
@@ -34,9 +34,10 @@ int test_Node() {
     std::cout << "n5(99) > n5(99) == " << (n5 > n5) << std::endl;
     std::cout << "\n";
 
-    std::cout << "n4.accuracy() == " << n4.accuracy() << '\n';
-    std::cout << "n5.accuracy() == " << n5.accuracy() << '\n';
-    std::cout << "n5 > n4 ? n5 : n4 == " << (n5 > n4 ? n5.accuracy() : n4.accuracy()) << std::endl;
+    // accuracy() multiplies by 100 to display % so undo for test
+    std::cout << "n4.accuracy() == " << n4.accuracy() / 100 << '\n';
+    std::cout << "n5.accuracy() == " << n5.accuracy() / 100 << '\n';
+    std::cout << "n5 > n4 ? n5 : n4 == " << (n5 > n4 ? n5.accuracy()/100 : n4.accuracy()/100) << std::endl;
     std::cout << "\n";
 
 
--- test/test_PriorityQueue.cpp
+++ test/test_PriorityQueue.cpp
@@ -8,25 +8,19 @@ int test_PriorityQueue() {
 
     std::priority_queue<Node> pq;
 
-
     // fill pq
     for(int i = 1; i < 11; i++)
     {
         acc = rand() % 100;
         printf("pushing with acc == %d\n", acc);
         pq.push( Node(acc) );
-
     }
 
-
-    // empty pq to console
+    // empty pq to console. accuracy() multiplies by 100 (%) so undo for test
     std::cout << "\npq: ";
     for(;!pq.empty(); pq.pop())
-        std::cout << pq.top().accuracy() << ' ';
+        std::cout << pq.top().accuracy() / 100 << ' ';
     std::cout << '\n';
-
-
-    
 
     std::cout << "\n";
 
